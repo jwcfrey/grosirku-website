17edf5e279261cb5f000cc663bcabff274caddff
Faturrahman <shen9k@gmail.com>
2024-07-07 20:06:13 +0700

    Display Total jumlah data di Admin Home (30) by Faturrahman

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -11,7 +11,7 @@
         <!-- Sidebar Navidation Menus-->
         <span class="heading">Main</span>
         <ul class="list-unstyled">
-                <li class="active"><a href="index.html"> <i class="icon-home"></i>Home </a></li>
+                <li><a href="{{url('admin/dashboard')}}"> <i class="icon-home"></i>Dashboard</a></li>
 
                 <li>
                   <a href="{{url('view_category')}}"> <i class="icon-grid"></i>Kategori</a>
