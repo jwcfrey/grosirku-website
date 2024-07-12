d2c4421f5d38dd9c4d03b71bf0419073dc251fc1
herukhoerdn <khoerudinheru56@gmail.com>
2024-07-12 18:31:30 +0700

    Menambahkan Tampilan pada fitur payment

--- resources/views/home/stripe.blade.php
+++ resources/views/home/stripe.blade.php
@@ -1,150 +1,133 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Laravel E-commerce payment</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" />
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <title>Laravel E-commerce Payment</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.min.css" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <style>
+        .credit-card-box {
+            border: 1px solid #e0e0e0;
+            border-radius: 10px;
+            padding: 20px;
+            box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+            background: #fff;
+        }
+        .panel-title {
+            font-weight: bold;
+            font-size: 1.2rem;
+            color: #333;
+        }
+        .btn-custom {
+            background-color: #4CAF50;
+            color: white;
+        }
+        .btn-custom:hover {
+            background-color: #45a049;
+        }
+    </style>
 </head>
 <body>
+<div class="container mt-5">
+    <h1 class="text-center">Payment Gateway - Isekai Store</h1>
     
-<div class="container">
-    
-    <h1>Payment Gateway isekai store.com <br/> </h1>
-    
-    <div class="row">
-        <div class="col-md-6 col-md-offset-3">
-            <div class="panel panel-default credit-card-box">
-                <div class="panel-heading display-table" >
-                        <h3 class="panel-title" >Payment Details</h3>
-                        <h4>Kamu harus membayar ${{ $value }}</h4>
+    <div class="row justify-content-center mt-4">
+        <div class="col-md-6">
+            <div class="credit-card-box">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Payment Details</h3>
+                    <h4>You need to pay ${{ $value }}</h4>
                 </div>
                 <div class="panel-body">
-    
+
                     @if (Session::has('success'))
                         <div class="alert alert-success text-center">
                             <a href="#" class="close" data-dismiss="alert" aria-label="close">×</a>
                             <p>{{ Session::get('success') }}</p>
                         </div>
                     @endif
-    
+
                     <form 
-                            role="form" 
-                            action="{{ route('stripe.post', $value) }}" 
-                            method="post" 
-                            class="require-validation"
-                            data-cc-on-file="false"
-                            data-stripe-publishable-key="{{ env('STRIPE_KEY') }}"
-                            id="payment-form">
+                        role="form" 
+                        action="{{ route('stripe.post', $value) }}" 
+                        method="post" 
+                        class="require-validation"
+                        data-cc-on-file="false"
+                        data-stripe-publishable-key="{{ env('STRIPE_KEY') }}"
+                        id="payment-form">
                         @csrf
-    
-                        <div class='form-row row'>
-                            <div class='col-xs-12 form-group required'>
-                                <label class='control-label'>Nama Kartu Anda</label> <input
-                                    class='form-control' size='4' type='text'>
+
+                        <div class="form-group">
+                            <label for="card-name">Nama pada Kartu</label>
+                            <input type="text" class="form-control" id="card-name" placeholder="Name on Card" required>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="card-number">Nomor Kartu</label>
+                            <input type="text" class="form-control card-number" id="card-number" placeholder="Card Number" autocomplete="off" required>
+                        </div>
+
+                        <div class="form-row">
+                            <div class="col-md-4 form-group">
+                                <label for="card-cvc">CVC</label>
+                                <input type="text" class="form-control card-cvc" id="card-cvc" placeholder="CVC" autocomplete="off" required>
+                            </div>
+                            <div class="col-md-4 form-group">
+                                <label for="card-expiry-month">Bulan Kedaluwarsa</label>
+                                <input type="text" class="form-control card-expiry-month" id="card-expiry-month" placeholder="MM" required>
+                            </div>
+                            <div class="col-md-4 form-group">
+                                <label for="card-expiry-year">Tahun Kedaluwarsa</label>
+                                <input type="text" class="form-control card-expiry-year" id="card-expiry-year" placeholder="YYYY" required>
                             </div>
                         </div>
-    
-                        <div class='form-row row'>
-                            <div class='col-xs-12 form-group card required'>
-                                <label class='control-label'>Nomor Kartu</label> <input
-                                    autocomplete='off' class='form-control card-number' size='20'
-                                    type='text'>
-                            </div>
+
+                        <div class="form-group hide error">
+                            <div class="alert alert-danger">Harap perbaiki kesalahan dan coba lagi.</div>
                         </div>
-    
-                        <div class='form-row row'>
-                            <div class='col-xs-12 col-md-4 form-group cvc required'>
-                                <label class='control-label'>CVC</label> <input autocomplete='off'
-                                    class='form-control card-cvc' placeholder='ex. 311' size='4'
-                                    type='text'>
-                            </div>
-                            <div class='col-xs-12 col-md-4 form-group expiration required'>
-                                <label class='control-label'>Bulan kadaluwarsa</label> <input
-                                    class='form-control card-expiry-month' placeholder='MM' size='2'
-                                    type='text'>
-                            </div>
-                            <div class='col-xs-12 col-md-4 form-group expiration required'>
-                                <label class='control-label'>Tahun Kedaluwarsa</label> <input
-                                    class='form-control card-expiry-year' placeholder='YYYY' size='4'
-                                    type='text'>
-                            </div>
-                        </div>
-    
-                        <div class='form-row row'>
-                            <div class='col-md-12 error form-group hide'>
-                                <div class='alert-danger alert'>Harap perbaiki kesalahan dan coba lagi.
-                                </div>
-                            </div>
-                        </div>
-    
-                        <div class="row">
-                            <div class="col-xs-12">
-                                <button class="btn btn-primary btn-lg btn-block" type="submit">Bayar Sekarang</button>
-                            </div>
-                        </div>
-                            
+
+                        <button type="submit" class="btn btn-custom btn-block">Bayar Sekarang</button>
                     </form>
                 </div>
             </div>        
         </div>
     </div>
-        
 </div>
-    
-</body>
-    
-<script type="text/javascript" src="https://js.stripe.com/v2/"></script>
-    
-<script type="text/javascript">
-  
+
+<script src="https://js.stripe.com/v2/"></script>
+<script>
 $(function() {
-  
-    /*------------------------------------------
-    --------------------------------------------
-    Stripe Payment Code
-    --------------------------------------------
-    --------------------------------------------*/
-    
     var $form = $(".require-validation");
-     
+
     $('form.require-validation').bind('submit', function(e) {
         var $form = $(".require-validation"),
-        inputSelector = ['input[type=email]', 'input[type=password]',
-                         'input[type=text]', 'input[type=file]',
-                         'textarea'].join(', '),
-        $inputs = $form.find('.required').find(inputSelector),
-        $errorMessage = $form.find('div.error'),
-        valid = true;
+            inputSelector = ['input[type=text]', 'input[type=file]', 'textarea'].join(', '),
+            $inputs = $form.find('.required').find(inputSelector),
+            $errorMessage = $form.find('div.error'),
+            valid = true;
         $errorMessage.addClass('hide');
-    
+
         $('.has-error').removeClass('has-error');
         $inputs.each(function(i, el) {
-          var $input = $(el);
-          if ($input.val() === '') {
-            $input.parent().addClass('has-error');
-            $errorMessage.removeClass('hide');
-            e.preventDefault();
-          }
+            var $input = $(el);
+            if ($input.val() === '') {
+                $input.parent().addClass('has-error');
+                $errorMessage.removeClass('hide');
+                e.preventDefault();
+            }
         });
-     
+
         if (!$form.data('cc-on-file')) {
-          e.preventDefault();
-          Stripe.setPublishableKey($form.data('stripe-publishable-key'));
-          Stripe.createToken({
-            number: $('.card-number').val(),
-            cvc: $('.card-cvc').val(),
-            exp_month: $('.card-expiry-month').val(),
-            exp_year: $('.card-expiry-year').val()
-          }, stripeResponseHandler);
+            e.preventDefault();
+            Stripe.setPublishableKey($form.data('stripe-publishable-key'));
+            Stripe.createToken({
+                number: $('.card-number').val(),
+                cvc: $('.card-cvc').val(),
+                exp_month: $('.card-expiry-month').val(),
+                exp_year: $('.card-expiry-year').val()
+            }, stripeResponseHandler);
         }
-    
     });
-      
-    /*------------------------------------------
-    --------------------------------------------
-    Stripe Response Handler
-    --------------------------------------------
-    --------------------------------------------*/
+
     function stripeResponseHandler(status, response) {
         if (response.error) {
             $('.error')
@@ -152,15 +135,13 @@ $(function() {
                 .find('.alert')
                 .text(response.error.message);
         } else {
-            /* token contains id, last4, and card type */
             var token = response['id'];
-                 
-            $form.find('input[type=text]').empty();
+            $form.find('input[type=text]').val('');
             $form.append("<input type='hidden' name='stripeToken' value='" + token + "'/>");
             $form.get(0).submit();
         }
     }
-     
 });
 </script>
+</body>
 </html>
